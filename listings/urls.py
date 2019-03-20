418e633ea32c6e0d10bd409ab7ce3cb4c114b08a
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2019-03-20 19:32:17 +0200

    Displayed all listings from the template

--- listings/urls.py
+++ listings/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="listings"),  # "" is equal to /listings
-    path("<int:listings_id>", views.listing, name="listing"),
+    path("<int:listing_id>", views.listing, name="listing"),
     path("search", views.search, name="search")
 ]
