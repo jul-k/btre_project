2876df080b9fa6df5a656376913cbd8eab7e42c3
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2019-03-22 18:11:19 +0200

    Search form filtering and displaying the right listings

--- listings/views.py
+++ listings/views.py
@@ -30,10 +30,54 @@ def listing(request, listing_id):
 
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # KEYWORDS search
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+
+    # CITY search
+
+    if 'city' in request.GET:
+        # server is searching for the name attr with 'city' value
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(
+                city__iexact=city)
+
+    # STATE search
+
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(
+                state__iexact=state)
+
+    # BEDROOMS search
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(
+                bedrooms__lte=bedrooms)
+
+    # PRICE search
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(
+                price__lte=price)
+
     context = {
         'state_choices': state_choices,
         'price_choices': price_choices,
-        'bedroom_choices': bedroom_choices
+        'bedroom_choices': bedroom_choices,
+        'listings': queryset_list
     }
 
     return render(request, 'listings/search.html', context)
