fa0d8b5c2549fccc0d3f9fde70051983ef9db5eb
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2019-03-27 16:07:06 +0200

    Created Contact Model & made Contacts migration

--- btre/settings.py
+++ btre/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'contacts.apps.ContactsConfig',
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
