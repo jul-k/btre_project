a9d5ceee5596f809e2b17ab13f46f256d6ad357c
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2019-04-11 16:37:26 +0300

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -148,3 +148,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'janeandjohndoe0000@gmail.com'
 EMAIL_HOST_PASSWORD = 'h7ke1SMOpVJP'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
