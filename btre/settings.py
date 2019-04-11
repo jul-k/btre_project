d4d54f2f0ff27f45103644df36de399c3d02b841
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2019-04-11 16:50:35 +0300

    Removed email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -142,13 +142,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'janeandjohndoe0000@gmail.com'
-EMAIL_HOST_PASSWORD = 'h7ke1SMOpVJP'
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
